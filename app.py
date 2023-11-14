9b86077f05d29d676f7471c30c187e0b507f680d
Joudy-sb <joudy.f.sabbagh@gmail.com>
2023-11-14 02:20:24 +0200

    draft prototype

--- app.py
+++ app.py
@@ -1,26 +1,24 @@
 from flask import Flask, redirect, render_template, request, session, url_for
 import random
 from string import ascii_uppercase
+from flask_socketio import SocketIO, leave_room, send, join_room
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "line&joods"
+socketio = SocketIO(app)
 
-room_user = list()
-room_keys = list()
+room_users = {}
+room_keys = set()
 
 def generate_key():
-    while True:
+    while True: 
         key = ""
         for i in range(0,4):
             key += random.choice(ascii_uppercase)
         if key not in room_keys:
+            room_keys.add(key)
             break
     return key
-
-# Route handler for the "/room" URL
-@app.route("/room")
-def room():
-    room_key = session.get("room")
-    return render_template("room.html", room_key = room_key)
 
 @app.route('/')
 def main():
@@ -30,18 +28,25 @@ def main():
 def create_room():
     return render_template('create_room.html')
 
+@app.route("/room")
+def room():
+    room_key = session.get("room")
+    return render_template("room.html", room_key=room_key)
+
 @app.route('/process_create_room', methods=['POST'])
 def process_create_room():
     user_name = request.form['name']
-    room_user.append(user_name)
     room_key = generate_key()
 
-    #session["name"] = user_name
+    room_users[room_key] = [user_name]
+
+    session["name"] = user_name
+    session["room"] = room_key
 
     return redirect(url_for("room"))
 
 @app.route('/join_room')
-def join_room():
+def join_room_view():
     return render_template('join_room.html')
 
 @app.route('/process_join_room', methods=['POST'])
@@ -50,12 +55,68 @@ def process_join_room():
     room_key = request.form['key']
 
     if room_key not in room_keys:
-        return render_template('join_room.html')
+        return render_template('join_room.html', error='Room does not exist.')
+    
+    room_users.setdefault(room_key, []).append(user_name)
     
     session["room"] = room_key
     session["name"] = user_name
     
     return redirect(url_for("room"))
+
+@socketio.on('connect')
+def connect():
+    user_name = session.get('name')
+    room_key = session.get('room')
+
+    if not user_name or not room_key:
+        return False 
+
+    if room_key not in room_keys:
+        send({"name": user_name, "message": "Room does not exist."}, room=room_key)
+        return False 
+
+    join_room(room=room_key)
+    send({"name": user_name, "message": "has entered the room"}, room=room_key)
+
+    print(f"{user_name} joined room {room_key}")
+
+@socketio.on('disconnect')
+def disconnect():
+    user_name = session.get("name")
+    room_key = session.get("room")
+
+    if not room_key:
+        print(f"{user_name} has disconnected and was not in any room.")
+        return False
+
+    if room_key in room_keys:
+        room_users[room_key].remove(user_name)
+
+        if not room_users[room_key]:
+            del room_users[room_key]
+            room_keys.remove(room_key)
+
+        send({"name": user_name, "message": "has left the room"}, room=room_key)
+
+    print(f"{user_name} left room {room_key}")
+
+@socketio.on('message')
+def message(data):
+    user_name = session.get("name")
+    room_key = session.get("room")
+
+    if room_key not in room_keys:
+        return False
     
-if __name__ == '__main__':
-    app.run()
+    content = {
+        "name": user_name,
+        "message": data['data']
+    }
+
+    send(content, to=room_key)
+    print(f"{user_name} said: {data['data']}")
+    
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
